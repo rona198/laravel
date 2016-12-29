fa3973abe44ce195b5161f61f4c863440e1fac38
rona198 <laven255@gmail.com>
2016-12-29 18:40:52 +0800

    Logging Users In

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,28 +3,39 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
+	public function getDashboard()
+	{
+		return view('dashboard');
+	}
+	
 	public function postSignUp(Request $request)
 	{
+		
 		$email = $request['email'];
 		$first_name = $request['first_name'];
 		$password = bcrypt($request['password']);
-		
 		$user = new User();
 		$user->email = $email;
 		$user->first_name = $first_name;
 		$user->password = $password;
 		
 		$user->save();
-				
-		return redirect()->route('dashboard');
+		Auth::login($user);
+		return redirect()->route('dashboard');	
 	}
-  
+
 	public function postSignIn(Request $request)
 	{
 		
-    }
-	
+		if ( Auth::attempt( ['email' => $request['email'], 'password' => $request['password'] ] ) ) {
+			return redirect()->route('dashboard');
+		}
+		return redirect()->back();
+	}	
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,13 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['web']], function (){
+	Route::get('/', function () {
+	    return view('welcome');
+	});
 
-Route::group(['middleware' => ['web']], function (){	
 	Route::post('/signup', [
 		'uses' => 'UserController@postSignUp',
 		'as' => 'signup'
+	]);
+
+	Route::post('/signin' ,	[
+		'uses' => 'UserController@postSignIn' ,
+    	 'as' => 'signin'   
+	]); 
+
+	Route::get('/dashboard', [
+		'uses' => 'UserController@getDashboard',
+		'as' => 'dashboard',
 	]);
 });
