c4c806dfc5eeb286948f46ba8d45d9a9e7046200
rona198 <laven255@gmail.com>
2016-12-30 09:56:18 +0800

    Post Validation & Outputting Errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,7 @@
 @endsection
 
 @section('content')
-    @if(count($errors) > 0)
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    @include('includes.message-block')
     </div>
     <div class="row">
         <div class="col-md-6">
